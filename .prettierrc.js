ee5c2a26c3ead9b6baebffd1633823e3f1fcb261
haydenk <hayden101@gmail.com>
2022-09-01 19:39:50 +1200

    added config files

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,4 @@
-{
+module.exports = {
   "printWidth": 300,
   "singleQuote": true,
   "tabWidth": 2,
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,13 +15,28 @@ export default {
   components: {
     ProductsView
   },
+  // object syntax is required
+  // when declaring default values for injections
+  inject: {
+    message: {
+      from: 'message', // this is optional if using the same key for injection
+      default: 'default value'
+    },
+    user: {
+      // use a factory function for non-primitive values that are expensive
+      // to create, or ones that should be unique per component instance.
+      default: () => ({ name: 'John' })
+    }
+  }
 }
+
 </script>
 
 <template>
   <div>
     <ProductsView :displaymessage='welcomeMsg' />
     <h2>HomeView</h2>
+    {{ user }}
   </div>
 
 </template>
