1d836e81ecd2969b4289f148a18960672a803b30
B Skjellerup <bindyskjellerup@gmail.com>
2022-09-02 22:11:46 +1200

    Added PageNotFound to Router

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,46 @@
+<script setup>
+
+
+
+</script>
+
+<template>
+
+   <div>
+    <h2>404 - This page does not exist</h2>
+    <h1>Whoopsie something went wrong!</h1>
+    <p>Please return home again. Yay!</p>
+  </div>
+
+  <router-link to="/" class="top">Return home</router-link>
+  <router-view />
+
+</template>
+
+<style lang="scss">
+
+  div {
+    height: 30rem;
+  }
+
+  h2 {
+    margin: auto 0;
+    align-items: center;
+    text-align: center;
+    padding: 20px;
+  }
+
+  h1 {
+    margin: auto 0;
+    align-items: center;
+    text-align: center;
+    padding: 20px;
+  }
+
+  p {
+    margin: auto 0;
+    align-items: center;
+    text-align: center;
+    padding: 20px;
+  }
+</style>
