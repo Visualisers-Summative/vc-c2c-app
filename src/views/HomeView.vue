6562984ef7ddba60cbd2a56d8f4d30601b1716f9
haydenk <hayden101@gmail.com>
2022-09-03 11:23:52 +1200

    axios working...

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,9 +9,17 @@
     <p>item: {{ item._id }}</p>
     <hr>
     <p>{{ item.imageUrl }}</p>
+    <p>{{ item.imgUrl }}</p>
     <img class="card_image" :src="image" alt="instruments" />
   </div> -->
 
+  <div v-for="(user, index) in users" :key="index">
+    <div>
+      <p>{{ user.avatar }}</p>
+      <img :src="user.avatar" alt="">
+    </div>
+
+  </div>
 
   <div>
     <Login />
@@ -33,6 +41,7 @@ import SignUp from '../components/SignUp.vue';
 import ProductCard from '../components/ProductCard.vue';
 import ProductService from '../services/ProductService.js';
 // import { get } from 'http';
+import axios from "axios";
 
 const api = 'https://vc-products.netlify.app/.netlify/functions/api/'
 
@@ -45,10 +54,11 @@ export default {
   data () {
     return {
       records: null,
-      // recordsTwo: [{
-      //   imgUrl: ''
-      // }],
-      image: ''
+      // recordsTwo: [],
+      // image: '',
+      data: {
+        users: []
+      }
     };
   },
   components: {
@@ -60,9 +70,22 @@ export default {
     // getImageBlob (imageUrl) {
     //   return axios.get(imageUrl).then(response => window.URL.createObjectURL(response.data))
     // }
+    getUser () {
+      const URL = 'https://reqres.in/api/users/'
+      axios
+        .get(URL)
+        .then(res => {
+          console.log(res.data);
+          this.users = res.data.data;
+          console.log("users: " + this.users);
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    }
   },
   created () {
-    ProductService.getProducts()
+    ProductService.getProducts({ responseType: "json" })
       .then(response => {
         // console.log(response.data);
         this.records = response.data
@@ -72,6 +95,9 @@ export default {
         console.log(error);
       })
   },
+  mounted () {
+    this.getUser()
+  }
 
 
 }
