73e33a5f71b3e50d1984c4c1bcca8da777039444
hayd0ss <75119039+hayd0ss@users.noreply.github.com>
2022-09-05 13:14:06 +1200

    Update ProductCard.vue

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -14,36 +14,6 @@
 
     </div>
   </router-link>
-  <!-- <div class="mdc-card">
-    <div class="mdc-card__primary-action">
-      <div class="mdc-card__media mdc-card__media--square">
-        <div class="mdc-card__media-content">Title</div>
-      </div>
-      ... additional primary action content ...
-      <div class="mdc-card__ripple"></div>
-    </div>
-    <div class="mdc-card__actions">
-      <div class="mdc-card__action-buttons">
-        <button class="mdc-button mdc-card__action mdc-card__action--button">
-          <div class="mdc-button__ripple"></div>
-          <span class="mdc-button__label">Action 1</span>
-        </button>
-        <button class="mdc-button mdc-card__action mdc-card__action--button">
-          <div class="mdc-button__ripple"></div>
-          <span class="mdc-button__label">Action 2</span>
-        </button>
-      </div>
-      <div class="mdc-card__action-icons">
-        <button class="material-icons mdc-icon-button mdc-card__action mdc-card__action--icon"
-          title="Share">share</button>
-        <button class="material-icons heart-outline mdc-icon-button mdc-card__action mdc-card__action--icon"
-          title="Favourite">heart-outline</button>
-
-      </div>
-    </div>
-  </div> -->
-
-
 </template>
 
 <script>
