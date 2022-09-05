d59cff9b21e025441eda6b2296becef7baec6921
haydenk <hayden101@gmail.com>
2022-09-05 14:36:52 +1200

    Header Components initial functions added

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,22 @@
 <script setup>
-
+import HeaderRow from '../src/components/HeaderRow.vue';
 
 
 </script>
 
 <template>
 
-
-  <!-- <div class="nav-links">
-    <router-link to="/" class="top">Buy</router-link> |
-    <router-link to="/Sell" class="top">Sell</router-link> |
-    <router-link to="/Profile" class="top">Profile</router-link>
-  </div> -->
+  <div class="header">
+    <HeaderRow />
+  </div>
 
   <div class="body">
     <router-view />
   </div>
 
-
 </template>
+
+
 
 <style scoped>
 </style>
