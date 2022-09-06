3c6933944da9f3194671aed3d64a9a58d7fd4ba6
haydenk <hayden101@gmail.com>
2022-09-06 14:15:37 +1200

    Changes to app - footer and added signup functionality

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,35 @@
-<script setup>
+<script>
+import HeaderRow from './components/HeaderRow.vue';
+import FooterRow from './components/FooterRow.vue';
 
-import FooterRow from '../src/components/FooterRow.vue'
-import HeaderRow from '../src/components/HeaderRow.vue';
+export default {
+  components: { FooterRow, HeaderRow },
+  name: 'App',
 
+  data: () => ({
+    // loggedUser: "guest"
+  }),
+  methods: {
+    // setLoggedUser(loggedInUser) {
+    //   this.loggedUser = loggedInUser;
+    // }
+  }
+};
 </script>
 
 <template>
-
-
-
 
   <div class="header">
     <HeaderRow />
   </div>
 
-
   <div class="body">
     <router-view />
   </div>
 
-
-
   <div class="footer">
     <FooterRow />
   </div>
-
-
 
 </template>
 
