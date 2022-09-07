5c3f3a29c65b73b2a8c3facd7ff20c4454788e05
haydenk <hayden101@gmail.com>
2022-09-07 15:49:29 +1200

    Product card imported

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="product-card-container">
-    <router-link :to="{ name: 'ProductDetails', params: { id: vinyls._id } }" class="top">
+    <router-link
+      :to="{ name: 'ProductDetails', params: { id: vinyls._id } }"
+      class="top"
+    >
       <div class="product-card">
-        <img :src="vinyls.imageUrl" alt="Record cover" />
+        <img
+          :src="vinyls.imageUrl"
+          alt="Record cover"
+        />
         <div class="product-info">
           <div class="artist-div">
             <p class="artist">{{ vinyls.artistName }}</p>
           </div>
           <p class="album">{{ vinyls.albumTitle }}</p>
-          <div class="genre" >
+          <div class="genre">
             <span>{{ vinyls.genre.join(', ') }}</span>
           </div>
           <p class="length">{{ vinyls.length.toUpperCase() }}</p>
@@ -22,7 +28,13 @@
         </div>
       </div>
     </router-link>
-    <svg @click="favourite" width="20" height="20" class="bi" viewBox="0 0 512 512">
+    <svg
+      @click="favourite"
+      width="20"
+      height="20"
+      class="bi"
+      viewBox="0 0 512 512"
+    >
       <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />
     </svg>
   </div>
@@ -46,29 +58,17 @@ export default {
   },
   methods: {
     favourite() {
-<<<<<<< HEAD
       event.target.classList.toggle('favourite')
     },
-=======
-      event.target.classList.toggle("favourite")
-    }
->>>>>>> 1e9e5aac16d73614c3d538827c0df37f19742e02
   },
   components: {
     SvgIcon,
   },
   filters: {
-<<<<<<< HEAD
-    uppercased() {
-      return this.value.toUpperCase()
+    titleise(value) {
+      return value.replace(/(?:^|\s|-)\S/g, x => x.toUpperCase())
     },
   },
-=======
-    titleise(value) {
-      return value.replace(/(?:^|\s|-)\S/g, x => x.toUpperCase());
-    }
-  }
->>>>>>> 1e9e5aac16d73614c3d538827c0df37f19742e02
 }
 </script>
 
