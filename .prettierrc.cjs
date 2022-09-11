f3dc5b75c5bd3b0f6fa57daced1792a1c98009af
haydenk <hayden101@gmail.com>
2022-09-11 22:05:36 +1200

    Pagination added

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-  "printWidth": 300,
+  "printWidth": 100,
   "singleQuote": true,
   "tabWidth": 2,
   "useTabs": false,
---
+++ src/components/PaginationComp.vue
@@ -0,0 +1,65 @@
+<template>
+  <!-- <div>  &#5171 </div> -->
+  <div
+    v-if="totalPages() > 0"
+    class="pagination-wrapper"
+  >
+ 
+    <span
+      v-if="showPreviousLink()"
+      class="pagination-btn"
+      @click="updatePage(currentPage - 1)"
+    >
+    <!-- prettier-ignore -->
+    &#5176 
+    </span>
+    {{ currentPage + 1 }} of {{ totalPages() }}
+    
+    <span
+      v-if="showNextLink()"
+      class="pagination-btn"
+      @click="updatePage(currentPage + 1)"
+    >
+     <!-- prettier-ignore -->
+     &#5171
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PaginationComp',
+  props: ['recordsArray', 'currentPage', 'pageSize'],
+  data() {
+    return {
+      pageTotal: '',
+    }
+  },
+  methods: {
+    updatePage(pageNumber) {
+      this.$emit('page:update', pageNumber)
+    },
+    totalPages() {
+      // console.log('length = ' + this.pageTotal)
+      // return
+      return Math.ceil(this.recordsArray.length / this.pageSize)
+    },
+    showPreviousLink() {
+      return this.currentPage == 0 ? false : true
+    },
+    showNextLink() {
+      return this.currentPage == this.totalPages() - 1 ? false : true
+    },
+  },
+  mounted() {
+    console.log(this.recordsArray.length)
+    // this.pageTotal = this.records.length
+  },
+}
+</script>
+
+<style>
+.pagination-btn {
+  cursor: pointer;
+}
+</style>
