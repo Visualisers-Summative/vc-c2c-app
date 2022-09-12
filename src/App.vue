14efb4e4f2ac7b5eacaec3c28d01aa4dd88263bf
B Skjellerup <bindyskjellerup@gmail.com>
2022-09-12 16:31:27 +1200

    Changed onmouseout to onclick

--- src/App.vue
+++ src/App.vue
@@ -65,7 +65,6 @@
           >Cart</span
         >
       </div>
-      <!-- | Cart -->
 
       <div class="box">
         <form name="search">
@@ -73,7 +72,7 @@
             type="text"
             class="input"
             name="txt"
-            onmouseout="this.value = ''; this.blur();"
+            onclick="this.value = ''; this.blur();"
           />
           <a href="#"
             ><img
