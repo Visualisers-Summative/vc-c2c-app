5420f366f55bed4b4d224855d1f0b0379b9b64e8
haydenk <hayden101@gmail.com>
2022-09-22 01:55:46 +1200

    vite.config build size increased

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),]
+  plugins: [vue(),],
+  build: {
+    chunkSizeWarningLimit: 2000,
+  },
 })
 
 // eslint()eslint()
